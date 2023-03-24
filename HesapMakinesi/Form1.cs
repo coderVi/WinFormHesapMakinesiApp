471e8e70f402bd5f3450f59b38c1d798d7057018
MONSTER <MONSTER@DESKTOP-OH4B9H3>
2023-03-24 20:42:23 +0300

    Proje dosyası ekle.

---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        int s1, s2, s3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s1 = Convert.ToInt32(textBox1.Text);
+            s2 = Convert.ToInt32(textBox2.Text);
+            s3 = s1 + s2;
+            label4.Text = s3.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s1 = Convert.ToInt32(textBox1.Text);
+            s2 = Convert.ToInt32(textBox2.Text);
+            s3 = s1 - s2;
+            label4.Text = s3.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            s1 = Convert.ToInt32(textBox1.Text);
+            s2 = Convert.ToInt32(textBox2.Text);
+            s3 = s1 * s2;
+            label4.Text = s3.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            s1 = Convert.ToInt32(textBox1.Text);
+            s2 = Convert.ToInt32(textBox2.Text);
+            s3 = s1 / s2;
+            label4.Text = s3.ToString();
+        }
+    }
+}
